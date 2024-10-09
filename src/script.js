91e9ee4130a6e2fa8b6cc044d0a43a93b3c61c04
biruk fekadu <birukefekadu632@gmail.com>
2024-10-09 21:58:43 +0300

    Toggle navbar display by clicking the 'bar' element
    Toggle navbar display by clicking the 'bar' element

--- src/script.js
+++ src/script.js
@@ -1,11 +1,20 @@
-const menuBar = document.querySelector('.menubar')
-menuBar.addEventListener('click', ()=>{
-    const navbarsm = document.querySelector('.navbarsm')
-    const currentDisplay = navbarsm.computedStyleMap.display
-    if(currentDisplay == 'none') {
-        navbarsm.computedStyleMap.display = 'block'
-    }
-    else{
-        navbarsm.computedStyleMap.display = 'none'
-    }
-})
+// const menuBar = document.querySelector('.menubar')
+// menuBar.addEventListener('click', ()=>{
+//     const navbarsm = document.querySelector('.navbarsm')
+//     const currentDisplay = navbarsm.computedStyleMap.display
+//     if(currentDisplay == 'none') {
+//         navbarsm.computedStyleMap.display = 'block'
+//     }
+//     else{
+//         navbarsm.computedStyleMap.display = 'none'
+//     }
+// })
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active')
+    })
+}
